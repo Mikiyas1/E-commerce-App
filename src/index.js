662324452bf4f055634355847e5491a95edc7d05
mikiyas simeneh <mikiyassimeneh147@gmail.com>
2022-04-09 01:20:37 -0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { getActiveElement } from "@testing-library/user-event/dist/utils";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById("root"));
